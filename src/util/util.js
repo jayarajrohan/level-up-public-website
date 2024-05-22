0ad06d85bc48cd9f8ef8f07342194a1f8c06aec3
jayarajrohan <iaminbest@gmail.com>
2024-05-22 18:28:19 +0530

    API callback

--- src/util/util.js
+++ src/util/util.js
@@ -1,17 +1,27 @@
 import axios from "axios";
+import router from "@/router";
 const BASE_URL = "https://level-up-nxxl.onrender.com/api";
 export function apiRequestManager(
   method = "get",
   url,
   data = {},
   params = {},
-  withCredentials = true
+  withCredentials = true,
+  callback
 ) {
-  return axios({
+  axios({
     method,
     url: `${BASE_URL}/${url}`,
     data,
     params,
     withCredentials,
-  });
+  })
+    .then((res) => callback(res))
+    .catch((err) => {
+      if (err.status === 401) {
+        router.push({ name: "Login" });
+        return;
+      }
+      callback(err);
+    });
 }
